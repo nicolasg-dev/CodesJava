8ed4abc2ff824707bb2f9ac2fa18e2c60897cf98
nicolasg-dev <ggnicolas28@gmail.com>
2026-09-12 22:03:27 -0300

    Primeira versão

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Verifica verifica = new Verifica();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o seu peso: ");
+
+        Double peso = scanner.nextDouble();
+
+        System.out.print("Digite a sua altura: ");
+
+        Double altura = scanner.nextDouble();
+
+        Double IMC = peso/ (altura*altura);
+        System.out.println("O IMC eh: " + IMC);
+
+        verifica.intervalos(IMC);
+
+        scanner.close();
+    }
+}
+
+class Verifica {
+    void intervalos(Double IMC){
+        if (IMC <= 18.5){
+            System.out.println("Abaixo do peso.");
+        } else if (IMC <= 24.9){
+            System.out.println("Peso normal.");    
+        } else if (IMC <= 29.9){
+            System.out.println("Sobrepeso.");  
+        } else if (IMC <= 34.9){
+            System.out.println("Obesidade Grau I."); 
+        } else if (IMC <= 39.9){
+            System.out.println("Obesidade Grau II."); 
+        } else {
+            System.out.println("Obesidade Grau III."); 
+        }
+    }
+}
